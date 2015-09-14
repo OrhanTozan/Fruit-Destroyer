2481e701fa71bf2cf2e4efc30daf3a76e69e20de
NahroT <xorhan007@gmail.com>
2015-09-14 17:46:04 +0200

    updates

--- core/src/com/nahroto/fruitdestroyer/screens/GameScreen.java
+++ core/src/com/nahroto/fruitdestroyer/screens/GameScreen.java
@@ -3,6 +3,7 @@ package com.nahroto.fruitdestroyer.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.nahroto.fruitdestroyer.Application;
@@ -23,14 +24,25 @@ public class GameScreen implements Screen
     {
         APP.camera.setToOrtho(false, Constants.V_WIDTH, Constants.V_HEIGHT);
         APP.camera.update();
-        FreeTypeFontGenerator generator = new FreeTypeFontGenerator()
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("district.otf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.genMipMaps = true;
+        parameter.minFilter = Texture.TextureFilter.Linear;
+        parameter.magFilter = Texture.TextureFilter.Linear;
+        parameter.size = 60;
+        font = generator.generateFont(parameter);
     }
 
     @Override
     public void render(float delta)
     {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        APP.batch.setProjectionMatrix(APP.camera.combined);
+        APP.batch.begin();
+        font.draw(APP.batch, "Loading", 100, 100);
+        APP.batch.end();
     }
 
     @Override
--- desktop/src/com/nahroto/fruitdestroyer/desktop/DesktopLauncher.java
+++ desktop/src/com/nahroto/fruitdestroyer/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.nahroto.fruitdestroyer.Application;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Fruit Destroyer";
+		config.width = 480;
+		config.height = 800;
 		new LwjglApplication(new Application(), config);
 	}
 }
