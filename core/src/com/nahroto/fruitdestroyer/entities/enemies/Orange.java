ba690c39d9647d6849cf44d63d9ce3e8ec974a2c
NahroT <xorhan007@gmail.com>
2015-09-29 21:39:43 +0200

    dsf

--- core/src/com/nahroto/fruitdestroyer/entities/enemies/Orange.java
+++ core/src/com/nahroto/fruitdestroyer/entities/enemies/Orange.java
@@ -6,8 +6,8 @@ public class Orange extends Enemy
 {
     public static final int COUNT = 30;
 
-    public Orange(Sprite sprite, int BOUNDING_X, int BOUNDING_Y, int BOUNDING_WIDTH, int BOUNDING_HEIGHT)
+    public Orange(Sprite sprite, Sprite redBar, Sprite greenBar, int BOUNDING_X, int BOUNDING_Y, int BOUNDING_WIDTH, int BOUNDING_HEIGHT)
     {
-        super(sprite, BOUNDING_X, BOUNDING_Y, BOUNDING_WIDTH, BOUNDING_HEIGHT);
+        super(sprite, redBar, greenBar, BOUNDING_X, BOUNDING_Y, BOUNDING_WIDTH, BOUNDING_HEIGHT);
     }
 }
