69ca8126eb5cb1f300f49ec5991ecbdc9918458b
NahroT <xorhan007@gmail.com>
2015-12-03 22:21:16 +0100

    no message

--- core/src/com/nahroto/fruitdestroyer/huds/BuyHud.java
+++ core/src/com/nahroto/fruitdestroyer/huds/BuyHud.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.nahroto.fruitdestroyer.Constants;
@@ -14,13 +16,15 @@ import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
 
 public class BuyHud extends Hud
 {
-    private final float EASE_TIME = 1f;
+    private final float EASE_TIME = 0.7f;
 
     private boolean isShowed;
     private boolean isEasing;
 
     private Image overlay;
     private Image blackShader;
+
+    private ImageButton extraAmmoButton;
 
     private Runnable toggleBuying;
     private Runnable toggleEasing;
@@ -71,12 +75,28 @@ public class BuyHud extends Hud
         blackShader.addAction(alpha(0f));
 
         overlay = new Image(gameScreenAtlas.findRegion("overlay"));
+
+        extraAmmoButton = new ImageButton(new TextureRegionDrawable(gameScreenAtlas.findRegion("moreAmmoUpgrade")), new TextureRegionDrawable(gameScreenAtlas.findRegion("moreAmmoUpgrade-down")));
+
         resetPosition();
 
         actors.add(blackShader);
         actors.add(overlay);
+        actors.add(extraAmmoButton);
 
         addAllActorsToStage();
+    }
+
+    @Override
+    public void update(float delta)
+    {
+        super.update(delta);
+        updateComponents();
+    }
+
+    private void updateComponents()
+    {
+        extraAmmoButton.setPosition(overlay.getX(Align.center), overlay.getY(Align.center), Align.bottom);
     }
 
     public void toggle()
