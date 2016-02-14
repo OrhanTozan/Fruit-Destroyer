ef864e328291a7196392f490ae7d43df077bd005
NahroTo <nahro.tozan@gmail.com>
2016-02-14 17:34:55 +0100

    no message

--- android/src/com/nahroto/fruitdestroyer/android/AndroidLauncher.java
+++ android/src/com/nahroto/fruitdestroyer/android/AndroidLauncher.java
@@ -1,8 +1,10 @@
 package com.nahroto.fruitdestroyer.android;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.badlogic.gdx.Gdx;
@@ -11,14 +13,20 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.games.Games;
+import com.google.example.games.basegameutils.GameHelper;
 import com.nahroto.fruitdestroyer.AdsController;
 import com.nahroto.fruitdestroyer.Application;
+import com.nahroto.fruitdestroyer.PlayServices;
 
-public class AndroidLauncher extends AndroidApplication implements AdsController
+public class AndroidLauncher extends AndroidApplication implements AdsController, PlayServices
 {
 	private static final String INTERSTITIAL_AD_UNIT_ID = "ca-app-pub-7980895899775610/2698922485";
 
 	InterstitialAd interstitialAd;
+
+	private GameHelper gameHelper;
+	private final static int requestCode = 1;
 
 	@Override
 	protected void onCreate (Bundle savedInstanceState)
@@ -26,7 +34,20 @@ public class AndroidLauncher extends AndroidApplication implements AdsController
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useImmersiveMode = true;
-		initialize(new Application(this), config);
+		gameHelper = new GameHelper(this, GameHelper.CLIENT_GAMES);
+		gameHelper.enableDebugLog(false);
+
+		GameHelper.GameHelperListener gameHelperListener = new GameHelper.GameHelperListener()
+		{
+			@Override
+			public void onSignInFailed(){ }
+
+			@Override
+			public void onSignInSucceeded(){ }
+		};
+
+		gameHelper.setup(gameHelperListener);
+		initialize(new Application(this, this), config);
 		setupAds();
 	}
 
@@ -70,5 +91,123 @@ public class AndroidLauncher extends AndroidApplication implements AdsController
 		NetworkInfo ni = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 
 		return (ni != null && ni.isConnected());
+	}
+
+	@Override
+	protected void onStart()
+	{
+		super.onStart();
+		gameHelper.onStart(this);
+	}
+
+	@Override
+	protected void onStop()
+	{
+		super.onStop();
+		gameHelper.onStop();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		super.onActivityResult(requestCode, resultCode, data);
+		gameHelper.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void signIn()
+	{
+		try
+		{
+			runOnUiThread(new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					gameHelper.beginUserInitiatedSignIn();
+				}
+			});
+		}
+		catch (Exception e)
+		{
+			Gdx.app.log("MainActivity", "Log in failed: " + e.getMessage() + ".");
+		}
+	}
+
+	@Override
+	public void signOut()
+	{
+		try
+		{
+			runOnUiThread(new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					gameHelper.signOut();
+				}
+			});
+		}
+		catch (Exception e)
+		{
+			Gdx.app.log("MainActivity", "Log out failed: " + e.getMessage() + ".");
+		}
+	}
+
+	@Override
+	public void rateGame()
+	{
+		String str = "Your PlayStore Link";
+		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(str)));
+	}
+
+	@Override
+	public void unlockAchievement()
+	{
+		Games.Achievements.unlock(gameHelper.getApiClient(),
+				getString(R.string.achievement_ach1));
+	}
+
+	@Override
+	public void submitScore(int highScore)
+	{
+		if (isSignedIn() == true)
+		{
+			Games.Leaderboards.submitScore(gameHelper.getApiClient(),
+					getString(R.string.leaderboard_high_scores_best_wave), highScore);
+		}
+	}
+
+	@Override
+	public void showAchievement()
+	{
+		if (isSignedIn() == true)
+		{
+			startActivityForResult(Games.Achievements.getAchievementsIntent(gameHelper.getApiClient()), requestCode);
+		}
+		else
+		{
+			signIn();
+		}
+	}
+
+	@Override
+	public void showScore()
+	{
+		if (isSignedIn() == true)
+		{
+			startActivityForResult(Games.Leaderboards.getLeaderboardIntent(gameHelper.getApiClient(),
+					getString(R.string.leaderboard_high_scores_best_wave)), requestCode);
+		}
+		else
+		{
+			signIn();
+		}
+	}
+
+	@Override
+	public boolean isSignedIn()
+	{
+		return gameHelper.isSignedIn();
 	}
 }
---
+++ core/src/com/nahroto/fruitdestroyer/PlayServices.java
@@ -0,0 +1,13 @@
+package com.nahroto.fruitdestroyer;
+
+public interface PlayServices
+{
+    public void signIn();
+    public void signOut();
+    public void rateGame();
+    public void unlockAchievement();
+    public void submitScore(int highScore);
+    public void showAchievement();
+    public void showScore();
+    public boolean isSignedIn();
+}
